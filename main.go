822ae52c4c6bb3197b86324ce1026f01f70468a8
Alik Kruhlienia <alikrugl11@gmail.com>
2024-07-31 19:30:34 +0300

    add new handler

--- main.go
+++ main.go
@@ -5,10 +5,14 @@ import "net/http"
 func main() {
 	mux := http.NewServeMux()
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
 
-	err := http.ListenAndServe(":8080", mux)
-    if err != nil {
-        panic(err)
-    }
+	fileServer := http.FileServer(http.Dir("."))
+	mux.Handle("/app/", http.StripPrefix("/app/", fileServer))
+
+	http.ListenAndServe(":8080", mux)
 }
